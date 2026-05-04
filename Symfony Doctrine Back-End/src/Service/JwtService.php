2bde0bacda6c65c406a885b91d647f0a545f40ee
DaanTimmerDCTerra <144139@student.dcterra.nl>
2026-05-04 12:38:59 +0200

    JWT tokens aanmaken en valideren

---
+++ Symfony Doctrine Back-End/src/Service/JwtService.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Service;
+
+use Firebase\JWT\JWT;
+use Firebase\JWT\Key;
+
+class JwtService
+{
+    public function __construct(private string $secret) {}
+
+    public function generate(int $userId, string $email): string
+    {
+        $payload = [
+            'sub'   => $userId,
+            'email' => $email,
+            'iat'   => time(),
+            'exp'   => time() + (60 * 60 * 24 * 7),
+        ];
+
+        return JWT::encode($payload, $this->secret, 'HS256');
+    }
+
+    public function decode(string $token): object
+    {
+        return JWT::decode($token, new Key($this->secret, 'HS256'));
+    }
+}
