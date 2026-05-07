50c8b611802725b97e3ebc7f9abefa78e45f71db
DaanTimmerDCTerra <144139@student.dcterra.nl>
2026-05-08 01:05:53 +0200

    Serializer service

--- Symfony Doctrine Back-End/src/Service/SerializerService.php
+++ Symfony Doctrine Back-End/src/Service/SerializerService.php
@@ -2,29 +2,20 @@
 
 namespace App\Service;
 
-use App\Entity\Comment;
 use App\Entity\Genre;
 use App\Entity\Title;
 use App\Entity\User;
 
 class SerializerService
 {
-    public function __construct(private TitleService $titleService) {}
-
-    public function serializeTitle(Title $title, ?User $currentUser = null, ?array $stats = null): array
+    public function serializeTitle(Title $title, ?User $currentUser = null): array
     {
-        $stats ??= $this->titleService->aggregateCommentStats($this->titleService->getMatchingTitles($title));
-
         return [
             'id' => $title->getId(),
             'name' => $title->getName(),
             'type' => $title->getType(),
             'year' => $title->getYear(),
             'watched' => (bool)$title->isWatched(),
-            'rating' => $title->getRating(),
-            'averageRating' => $stats['averageRating'],
-            'commentCount' => $stats['commentCount'],
-            'public' => (bool)$title->isPublic(),
             'favorite' => (bool)$title->isFavorite(),
             'owner' => $title->getUser()?->getEmail(),
             'ownedByCurrentUser' => $currentUser && $title->getUser() && $title->getUser()->getId() === $currentUser->getId(),
@@ -33,17 +24,6 @@ class SerializerService
                 'name' => $g->getName(),
             ])->toArray(),
             'thumbnail' => $title->getThumbnail() ? '/uploads/titles/' . $title->getThumbnail() : null,
-        ];
-    }
-
-    public function serializeComment(Comment $comment): array
-    {
-        return [
-            'id' => $comment->getId(),
-            'content' => $comment->getContent(),
-            'rating' => $comment->getRating(),
-            'createdAt' => $comment->getCreatedAt()->format('Y-m-d H:i'),
-            'user' => $comment->getUser()->getEmail(),
         ];
     }
 
